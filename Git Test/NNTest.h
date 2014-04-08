045f265640b315a6d58b0502912e0e34eb018fa6
Nathan <nathan.nguyen@invesco.com>
2014-04-08 12:42:17 -0500

    added a test class

---
+++ Git Test/NNTest.h
@@ -0,0 +1,13 @@
+//
+//  NNTest.h
+//  Git Test
+//
+//  Created by Nathan Nguyen on 4/8/14.
+//  Copyright (c) 2014 nigmia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NNTest : NSObject
+
+@end
---
+++ Git Test/NNTest.m
@@ -0,0 +1,13 @@
+//
+//  NNTest.m
+//  Git Test
+//
+//  Created by Nathan Nguyen on 4/8/14.
+//  Copyright (c) 2014 nigmia. All rights reserved.
+//
+
+#import "NNTest.h"
+
+@implementation NNTest
+
+@end
